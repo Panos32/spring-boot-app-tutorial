c0dfa1fd2e8d269e8fdf9e74a04d8fee2c1e2fee
Panos32 <97842668+Panos32@users.noreply.github.com>
2023-07-13 12:35:31 +0300

    A simple application using H2 Database

--- content-calendar/src/main/java/dev/panos/contentcalendar/config/DataLoader.java
+++ content-calendar/src/main/java/dev/panos/contentcalendar/config/DataLoader.java
@@ -1,12 +1,32 @@
 package dev.panos.contentcalendar.config;
 
-import org.springframework.boot.CommandLineRunner;
+import java.io.InputStream;
+import java.util.List;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.panos.contentcalendar.model.Content;
+import dev.panos.contentcalendar.repository.ContentRepository;
+
+@Component
 public class DataLoader implements CommandLineRunner {
+
+    private final ContentRepository repository;
+    private final ObjectMapper objectMapper;
+
+    public DataLoader(ContentRepository repository, ObjectMapper objectMapper) {
+        this.repository = repository;
+        this.objectMapper = objectMapper;
+    }
 
     @Override
     public void run(String... args) throws Exception{
-        System.out.println();
+        try(InputStream inputStream = TypeReference.class.getResourceAsStream("/data/content.json")) {
+            repository.saveAll(objectMapper.readValue(inputStream, new TypeReference<List<Content>>(){}));
+        }
     }
-    
 }
